3b7d9a714fca2af8b34313ec514d904a0f60c7ca
kkojae <jaejeung3210@gmail.com>
2022-04-19 21:27:32 +0900

    feat: 계산기 기능 구현

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,14 +1,108 @@
 import "./App.css";
-
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstNumber: 0,
+      operation: "",
+      secondNumber: "",
+    };
+
+    this.calculateScreenElement = createRef();
+  }
+
+  handleDigitButton = ({ target }) => {
+    if (this.state.operation) {
+      const prevNumber = this.state.secondNumber;
+      this.setState({
+        secondNumber: Number(prevNumber + target.textContent),
+      });
+
+      return;
+    }
+
+    const prevNumber = this.state.firstNumber;
+
+    this.setState({
+      firstNumber: !isNaN(prevNumber)
+        ? Number(prevNumber + target.textContent)
+        : target.textContent,
+    });
+  };
+
+  handleOperationButton = ({ target }) => {
+    if (target.textContent === "=" && this.state.secondNumber !== "") {
+      const resultNumber = this.calculateResultNumber();
+      console.log(resultNumber);
+
+      this.setState({
+        firstNumber: Number.isFinite(Number(resultNumber))
+          ? Number(resultNumber)
+          : "오류",
+        operation: "",
+        secondNumber: "",
+      });
+
+      return;
+    }
+
+    this.setState({
+      operation: target.textContent,
+    });
+  };
+
+  handleAllClear = () => {
+    this.setState({
+      firstNumber: 0,
+      operation: "",
+      secondNumber: "",
+    });
+  };
+
+  calculateResultNumber = () => {
+    const firstNumber = Number(this.state.firstNumber);
+    const secondNumber = Number(this.state.secondNumber);
+
+    const calculateMethod = {
+      X: firstNumber * secondNumber,
+      "/": firstNumber / secondNumber,
+      "+": firstNumber + secondNumber,
+      "-": firstNumber - secondNumber,
+    };
+
+    return calculateMethod[this.state.operation] || firstNumber;
+  };
+
+  convertToLocaleString = (number) => number.toLocaleString("ko-KR");
+
+  componentDidUpdate() {
+    console.log(this.state);
+    const calculateScreenElement = this.calculateScreenElement.current;
+
+    if (this.state.secondNumber === "") {
+      calculateScreenElement.textContent = this.convertToLocaleString(
+        this.state.firstNumber
+      );
+
+      return;
+    }
+
+    calculateScreenElement.textContent = this.convertToLocaleString(
+      this.state.secondNumber
+    );
+  }
+
   render() {
     return (
       <div className="app">
         <div className="calculator">
-          <h1 className="total">0</h1>
-          <div className="digits flex">
+          <h1 className="total" ref={this.calculateScreenElement}>
+            0
+          </h1>
+          <div className="digits flex" onClick={this.handleDigitButton}>
             <button className="digit">9</button>
             <button className="digit">8</button>
             <button className="digit">7</button>
@@ -21,9 +115,14 @@ export default class App extends Component {
             <button className="digit">0</button>
           </div>
           <div className="modifiers subgrid">
-            <button className="modifier">AC</button>
+            <button className="modifier" onClick={this.handleAllClear}>
+              AC
+            </button>
           </div>
-          <div className="operations subgrid">
+          <div
+            className="operations subgrid"
+            onClick={this.handleOperationButton}
+          >
             <button className="operation">/</button>
             <button className="operation">X</button>
             <button className="operation">-</button>
